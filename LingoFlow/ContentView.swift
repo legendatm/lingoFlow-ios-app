42f33d305559742cb646d58216c97a3c494fcbf9
chen <pom_114@163.com>
2025-12-29 20:23:44 +0800

    Initial Commit

---
+++ LingoFlow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LingoFlow
+//
+//  Created by Chen Desheng on 2025/12/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LingoFlow/LingoFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  LingoFlowApp.swift
+//  LingoFlow
+//
+//  Created by Chen Desheng on 2025/12/29.
+//
+
+import SwiftUI
+
+@main
+struct LingoFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
